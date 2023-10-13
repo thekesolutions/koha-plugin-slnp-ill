cb2160a191d83973bd07d76c037ea27a1f17915e
Tomas Cohen Arazi <tomascohen@theke.io>
2023-10-13 13:01:06 -0300

    Issue #106: Add 2.1 through 2.3 handling from the spec
    This patch implements (partially) up to 2.3. It is not clear in the spec
    how to handle the no-items use case. There's a contradiction in it. I
    went with the bullets.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Plugin/Com/Theke/SLNP/lib/SLNP/Commands/Bestellung.pm
+++ Koha/Plugin/Com/Theke/SLNP/lib/SLNP/Commands/Bestellung.pm
@@ -25,8 +25,11 @@ use utf8;
 use C4::Context;
 
 use Koha::Database;
+
+use Koha::Biblios;
 use Koha::Illrequests;
 use Koha::Plugin::Com::Theke::SLNP;
+use Koha::SearchEngine::Search;
 
 use Scalar::Util qw(blessed);
 use Try::Tiny;
@@ -40,128 +43,171 @@ sub SLNPFLBestellung {
 
     if ( $cmd->{'req_valid'} == 1 ) {
 
-        my $illrequest      = Koha::Illrequest->new();
-        my $slnp_illbackend = $illrequest->load_backend("SLNP");    # this is still $illrequest
+        if ( $params->{BsTyp} eq 'AFL' ) {
+            # 2.1 Check PPN number (TitelId)
+            if ( $params->{TitelId} ) {
+                # Local title => checks and handling
+                my $searcher = Koha::SearchEngine::Search->new({ index => $Koha::SearchEngine::BIBLIOS_INDEX });
+                my ( $err, $result, $count ) = $searcher->simple_search_compat( 'Control-number:' . $params->{TitelId}, 0, 1 );
 
-        my $args = { stage => 'commit' };
+                if ($count) {    # We have the record, next checks
+                    # 2.2  Check if is a copy
+                    if ( defined $params->{BsTyp2} && $params->{BsTyp2} eq 'C' ) { # Is a copy => accept request
+                        # FIXME: What about 'V'
+                        request_accepted( $cmd, $params->{ExternReferenz} );
+                    }
+                    else {
+                        # pick the first record in the resultset
+                        my $record = $result->[0];
+                        my $biblio_id = $searcher->extract_biblionumber( $record );
 
-        if (
-            (
-                defined $params->{AufsatzAutor}
-                && length( $params->{AufsatzAutor} )
-            )
-            || ( defined $params->{AufsatzTitel}
-                && length( $params->{AufsatzTitel} ) )
-          )
-        {
-            # FIXME: We shouldn't use 'medium' https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=21833#c4
-            $args->{medium} = 'copy';
-        }
-        else {
-            # FIXME: We shouldn't use 'medium' https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=21833#c4
-            $args->{medium} = 'loan';
-        }
-        $args->{orderid} = $params->{BestellId};
-
-        # fields for table illrequestattributes
-        my $attribute_mapping = attribute_mapping();
-        foreach my $attribute ( keys %{$attribute_mapping} ) {
-            if ( defined $params->{$attribute}
-                && length( $params->{$attribute} ) )
-            {
-                $args->{attributes}->{ $attribute_mapping->{$attribute} } =
-                  $params->{$attribute};
+                        my $biblio = Koha::Biblios->find( $biblio_id );
+                        # 2.3 Does the record have items?
+                        if ( $biblio->items->count ) { # No items => accept request
+                            request_accepted( $cmd, $params->{ExternReferenz} );
+                        }
+                        else { # no items => reject
+                            $cmd->{req_valid} = 0;
+                            $cmd->{err_type}  = 'NO_ITEMS_FOUND';
+                            $cmd->{err_text}  = "Es existieren keine bestellbaren Exemplare";
+                        }
+                    }
+                    # FIXME: we need to be able to determine if it comes from the union catalog
+                } else {
+                    $cmd->{req_valid} = 0;
+                    $cmd->{err_type}  = 'RECORD_NOT_FOUND';
+                    $cmd->{err_text}  = "Es existieren keine Exemplare";
+                }
+            }
+            else {# No PPN number => accept request. Processing takes place on the portal
+                request_accepted( $cmd, $params->{ExternReferenz} );
             }
         }
+        else {
 
-        $args->{attributes}->{type} = ( $args->{medium} eq 'copy' ) ? 'Kopie' : 'Leihe';
-        # Deal with AusgabeOrt => pickup_location translation
-        my $default_pickup_location = $configuration->{default_ill_branch} // 'CPL';
-        my $pickup_location_description = $args->{attributes}->{pickup_location_description};
-        my $pickup_location = ( $pickup_location_description and $configuration->{pickup_location_mapping}->{$pickup_location_description} )
-                            ? $configuration->{pickup_location_mapping}->{$pickup_location_description}
-                            : $default_pickup_location;
+            my $illrequest      = Koha::Illrequest->new();
+            my $slnp_illbackend = $illrequest->load_backend("SLNP");    # this is still $illrequest
 
-        # fields for table illrequests
-        $args->{branchcode} = $pickup_location;
-        $args->{attributes}->{pickup_location} = $pickup_location;
+            my $args = { stage => 'commit' };
 
-        my $backend_result;
+            if (
+                (
+                    defined $params->{AufsatzAutor}
+                    && length( $params->{AufsatzAutor} )
+                )
+                || ( defined $params->{AufsatzTitel}
+                    && length( $params->{AufsatzTitel} ) )
+            )
+            {
+                # FIXME: We shouldn't use 'medium' https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=21833#c4
+                $args->{medium} = 'copy';
+            }
+            else {
+                # FIXME: We shouldn't use 'medium' https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=21833#c4
+                $args->{medium} = 'loan';
+            }
+            $args->{orderid} = $params->{BestellId};
 
-        try {
-            Koha::Database->new->schema->txn_do(
-                sub {
-                    $backend_result = $slnp_illbackend->backend_create($args);
+            # fields for table illrequestattributes
+            my $attribute_mapping = attribute_mapping();
+            foreach my $attribute ( keys %{$attribute_mapping} ) {
+                if ( defined $params->{$attribute}
+                    && length( $params->{$attribute} ) )
+                {
+                    $args->{attributes}->{ $attribute_mapping->{$attribute} } =
+                    $params->{$attribute};
+                }
+            }
 
-                    if (   $backend_result->{error} ne '0'
-                        || !defined $backend_result->{value}
-                        || !defined $backend_result->{value}->{request}
-                        || !$backend_result->{value}->{request}->illrequest_id()
-                      )
-                    {
-                        # short-circuit, force rollback
-                        SLNP::Exception->throw("Error creating the illrequest object");
+            $args->{attributes}->{type} = ( $args->{medium} eq 'copy' ) ? 'Kopie' : 'Leihe';
+            # Deal with AusgabeOrt => pickup_location translation
+            my $default_pickup_location = $configuration->{default_ill_branch} // 'CPL';
+            my $pickup_location_description = $args->{attributes}->{pickup_location_description};
+            my $pickup_location = ( $pickup_location_description and $configuration->{pickup_location_mapping}->{$pickup_location_description} )
+                                ? $configuration->{pickup_location_mapping}->{$pickup_location_description}
+                                : $default_pickup_location;
+
+            # fields for table illrequests
+            $args->{branchcode} = $pickup_location;
+            $args->{attributes}->{pickup_location} = $pickup_location;
+
+            my $backend_result;
+
+            try {
+                Koha::Database->new->schema->txn_do(
+                    sub {
+                        $backend_result = $slnp_illbackend->backend_create($args);
+
+                        if (   $backend_result->{error} ne '0'
+                            || !defined $backend_result->{value}
+                            || !defined $backend_result->{value}->{request}
+                            || !$backend_result->{value}->{request}->illrequest_id()
+                        )
+                        {
+                            # short-circuit, force rollback
+                            SLNP::Exception->throw("Error creating the illrequest object");
+                        }
+
+                        my $prefix = $configuration->{pfl_number_prefix} // '';
+
+                        $cmd->{'rsp_para'}->[0] = {
+                            'resp_pnam' => 'PFLNummer',
+                            'resp_pval' => $prefix . $backend_result->{value}->{request}->illrequest_id()
+                        };
+
+                        $cmd->{'rsp_para'}->[1] = {
+                            'resp_pnam' => 'OKMsg',
+                            'resp_pval' => 'ILL request successfully inserted.'
+                        };
                     }
+                );
+            }
+            catch {
+                $cmd->{'req_valid'} = 0;
 
-                    my $prefix = $configuration->{pfl_number_prefix} // '';
-
-                    $cmd->{'rsp_para'}->[0] = {
-                        'resp_pnam' => 'PFLNummer',
-                        'resp_pval' => $prefix . $backend_result->{value}->{request}->illrequest_id()
-                    };
-
-                    $cmd->{'rsp_para'}->[1] = {
-                        'resp_pnam' => 'OKMsg',
-                        'resp_pval' => 'ILL request successfully inserted.'
-                    };
-                }
-            );
-        }
-        catch {
-            $cmd->{'req_valid'} = 0;
-
-            if ( blessed $_ ) { # An exception
-                if ( $_->isa('SLNP::Exception::PatronNotFound') ) {
-                    $cmd->{'err_type'} = 'PATRON_NOT_FOUND';
-                    $cmd->{'err_text'} = "No patron found having cardnumber '"
-                    . scalar $params->{BenutzerNummer} . "'.";
-                }
-                elsif ( $_->isa('SLNP::Exception::MissingParameter') ) {
-                    $cmd->{err_type} = 'SLNP_MAND_PARAM_LACKING';
-                    $cmd->{err_text} = 'Mandatory parameter missing: ' . $_->param;
-                }
-                elsif ( $_->isa('SLNP::Exception::BadConfig') ) {
-                    $cmd->{err_type} = 'INTERNAL_SERVER_ERROR';
-                    $cmd->{err_text} = 'Internal server error';
-                    $cmd->{warn} =
-                         'Configuration problem with mandatory parameter '
-                        . $_->param
-                        . ' (' . $_->value . ')';
-                }
-                elsif ( $_->isa('SLNP::Exception') ) {
-                    $cmd->{err_type} = 'INTERNAL_SERVER_ERROR';
-                    $cmd->{err_text} = 'Internal server error';
-                    $cmd->{warn} = "Uncaught exception: $_";
+                if ( blessed $_ ) { # An exception
+                    if ( $_->isa('SLNP::Exception::PatronNotFound') ) {
+                        $cmd->{'err_type'} = 'PATRON_NOT_FOUND';
+                        $cmd->{'err_text'} = "No patron found having cardnumber '"
+                        . scalar $params->{BenutzerNummer} . "'.";
+                    }
+                    elsif ( $_->isa('SLNP::Exception::MissingParameter') ) {
+                        $cmd->{err_type} = 'SLNP_MAND_PARAM_LACKING';
+                        $cmd->{err_text} = 'Mandatory parameter missing: ' . $_->param;
+                    }
+                    elsif ( $_->isa('SLNP::Exception::BadConfig') ) {
+                        $cmd->{err_type} = 'INTERNAL_SERVER_ERROR';
+                        $cmd->{err_text} = 'Internal server error';
+                        $cmd->{warn} =
+                            'Configuration problem with mandatory parameter '
+                            . $_->param
+                            . ' (' . $_->value . ')';
+                    }
+                    elsif ( $_->isa('SLNP::Exception') ) {
+                        $cmd->{err_type} = 'INTERNAL_SERVER_ERROR';
+                        $cmd->{err_text} = 'Internal server error';
+                        $cmd->{warn} = "Uncaught exception: $_";
+                    }
+                    else {
+                        $cmd->{'err_type'} = 'ILLREQUEST_NOT_CREATED';
+                        $cmd->{'err_text'} =
+                            "The Koha illrequest for the title '"
+                        . scalar $params->{Titel}
+                        . "' could not be created. ($_)";
+                    }
                 }
                 else {
                     $cmd->{'err_type'} = 'ILLREQUEST_NOT_CREATED';
                     $cmd->{'err_text'} =
                         "The Koha illrequest for the title '"
                     . scalar $params->{Titel}
-                    . "' could not be created. ($_)";
+                    . "' could not be created. ("
+                    . scalar $backend_result->{status} . ' '
+                    . scalar $backend_result->{message} . ")";
                 }
-            }
-            else {
-                $cmd->{'err_type'} = 'ILLREQUEST_NOT_CREATED';
-                $cmd->{'err_text'} =
-                    "The Koha illrequest for the title '"
-                  . scalar $params->{Titel}
-                  . "' could not be created. ("
-                  . scalar $backend_result->{status} . ' '
-                  . scalar $backend_result->{message} . ")";
-            }
-        };
-    }
+            };
+        }
+        }
 
     return $cmd;
 }
@@ -193,6 +239,30 @@ sub attribute_mapping {
         Auflage        => 'issue', # copy case
         Heft           => 'issue', # loan case
     };
+}
+
+=head3 request_accepted
+
+    request_accepted( $cmd, $pflnummer );
+
+Makes I<$cmd> carry the right information for a successful request.
+
+=cut
+
+sub request_accepted {
+    my ( $cmd, $pflnummer ) = @_;
+
+    $cmd->{rsp_para}->[0] = {
+        resp_pnam => 'PFLNummer',
+        resp_pval => $pflnummer,
+    };
+
+    $cmd->{rsp_para}->[1] = {
+        resp_pnam => 'OKMsg',
+        resp_pval => 'Bestellung wird bearbeitet',
+    };
+
+    return $cmd;
 }
 
 1;
